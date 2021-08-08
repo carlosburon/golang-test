4c1186bb11884af18a5eec55d6bc24cb0bd216ff
carlos <carlosburon@gmail.com>
2021-08-08 20:47:33 +0200

    included array of products as a mockup db

--- sre-challenge/main.go
+++ sre-challenge/main.go
@@ -6,6 +6,14 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	Code  string
+	Name  string
+	Price float32
+}
+
+var Products []Product
+
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Lana Merchandising</h1>")
 }
@@ -23,5 +31,14 @@ func handleRequests() {
 func main() {
 
 	fmt.Println("Server Starting...")
+
+	fmt.Println("Loading Products...")
+	Products = []Product{
+		{Code: "PEN", Name: "Lana Pen", Price: 5.0},
+		{Code: "TSHIRT", Name: "Lana T-Shirt", Price: 20.0},
+		{Code: "MUG", Name: "Lana Coffee Mug", Price: 7.5},
+	}
+
+	fmt.Println("Starting Router...")
 	handleRequests()
 }
