95b47f64c4e53d18fef246671739233892bf84a2
carlos <carlosburon@gmail.com>
2021-08-14 19:45:21 +0200

    cleanup

--- docker-commands.sh
+++
@@ -1,9 +0,0 @@
-
-sudo docker run -d   --name roach   --hostname db   --network mynet   -p 26257:26257   -p 8080:8080   -v roach:/cockroach/cockroach-data   cockroachdb/cockroach:latest-v20.1 start-single-node   --insecure
-
-sudo docker exec -it roach ./cockroach sql --insecure
-
-CREATE USER postgres;
-GRANT ALL ON DATABASE postgres TO postgres;
-
-sudo docker run -it --rm -d --network mynet --name lana-sre-challenge -p 3000:3000 lana-sre-challenge
